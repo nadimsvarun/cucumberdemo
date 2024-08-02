681b026daf860259921c6e28acd447686b1e98a0
Varun Nadimpalli <varun.nadimpalli@nwbrd.com>
2024-08-02 19:07:06 +0530

    added runner class for serenity report

---
+++ src/test/java/parallel/serenityrunner.java
@@ -0,0 +1,23 @@
+package parallel;
+
+
+
+//import net.serenitybdd.cucumber.CucumberWithSerenity;
+//import org.junit.runner.RunWith;
+//import io.cucumber.junit.CucumberOptions;
+//
+//@RunWith(CucumberWithSerenity.class)
+//@CucumberOptions(features = {"src/test/resources/features"},
+//        glue={"parallel","hooks"},
+//        plugin={"pretty", "html:target/cucumber-report.html",
+//                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+//                "rerun:target/failedrerun.txt"},
+//        monochrome = true,
+//        tags="@Datatable or @Scenariooutline"
+//
+//
+//)
+public class serenityrunner {
+
+
+}
